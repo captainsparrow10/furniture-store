295e1e2d5c232deaaa5cfb7edaafb27195b95ff1
captainsparrow10 <javier1009rm@gmail.com>
2023-12-23 11:45:05 -0500

    related api finish

--- src/app/api/shop/[id]/[...tags]/route.ts
+++ src/app/api/shop/[id]/[...tags]/route.ts
@@ -6,25 +6,37 @@ import { shopItemsInterface } from '@/utils/Interfaces'
 
 export async function GET(req: NextApiRequest, context: any) {
 	const { id, tags } = context.params
-	const tag = tags.map((tag: any) => `'${tag}'`)
+	const [tag1, tag2, tag3] = tags
 	const items: shopItemsInterface[] = await client.fetch(groq`*[
     _type == 'product' &&
     _id != '${id}' &&
-    references(*[_type == 'tag' && name in [${tag}]]._id)
+    (
+      (
+        '${tag1}' in tags[]->name &&
+        '${tag2}' in tags[]->name
+      ) ||
+      (
+        '${tag1}' in tags[]->name &&
+        '${tag3}' in tags[]->name
+      ) ||
+      (
+        '${tag2}' in tags[]->name &&
+        '${tag3}' in tags[]->name
+      )
+    )
   ]{
     _id,
+    name,
+    price,
+    "colors": *[
+      _type == 'color' && 
+      _id in ^.colors[]._ref
+    ]{
+      _id,
       name,
-      price,
-      "colors": 
-        *[
-          _type == 'color' 
-          && 
-          _id in ^.colors[]._ref]{
-          _id,
-            name,
-            urlList[]
-          }
-  }
+      urlList[]
+    }
+  }  
   `)
 	return NextResponse.json(items)
 }
