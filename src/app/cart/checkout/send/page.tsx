6931254136ca1cae920db366c09290ba3b39ddee
captainsparrow10 <javier1009rm@gmail.com>
2024-01-11 17:54:27 -0500

    local dev

--- src/app/cart/checkout/send/page.tsx
+++ src/app/cart/checkout/send/page.tsx
@@ -1,19 +1,7 @@
-'use client'
 import { CheckCircleIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
 
 export default function SendPage() {
-	const router = useRouter()
-	const [redirect, setRedirect] = useState(false)
-
-	if (!redirect) {
-		setTimeout(() => {
-			setRedirect(true)
-			router.push('/')
-		}, 10000)
-	}
 	return (
 		<div className="flex justify-center items-center  h-screen w-full px-6 lg:px-12 py-16 3xl:px-24">
 			<Link href="/">
