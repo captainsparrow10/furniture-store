1f48297391705501568527f653e7e75bbaa5784c
captainsparrow10 <javier1009rm@gmail.com>
2024-01-12 12:14:09 -0500

    prueba de sesion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import Picks from '@/components/pages/home/Picks'
 import TopPicks from '@/components/pages/home/TopPicks'
 import NewPick from '@/components/pages/home/NewPick'
 import Services from '@/lib/services/Services'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/services/Auth'
 
 export default async function page() {
 	const home: any = await Services.shop.getPresentation()
+	const session = await getServerSession(authOptions)
+	console.log(session)
 
 	return (
 		<main className="relative">
